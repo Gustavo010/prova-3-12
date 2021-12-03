40f95038588aa6a3a1aaab41da369353c2d7d4d0
Gustavo Henrique <82879034+Gustavo010@users.noreply.github.com>
2021-12-03 16:40:35 -0300

    Update pesca_controll.js

--- prova3do12BE/controll/pesca_controll.js
+++ prova3do12BE/controll/pesca_controll.js
@@ -1,70 +1,65 @@
 const { con } = require("./mysql_controll.js")
-const modelo = require('../model/pesca_modell.js')
+const modelo = require('../model/pesca_model.js')
 
-const getPesca = (req,res) => {
-    console.log("oi")
+const all = (req,res) => {
     let string = 'select * from pesca'
     con.query(string, (err, result)=>{
         res.json(result)
     })
 }
-const getPescaId = (req,res) => {
+
+const get_id = (req,res) => {
     let string = 'select * from pesca where id = '+ req.params.id
     con.query(string, (err, result)=>{
         res.json(result)
     })
 }
 
-const postPesca = (req, res) => {
-
-    let id = req.body.id
-    let cidade = '\'' + req.body.cidade + '\''
-    let quantidade = req.body.quantidade
-
-    let string = `insert into pesca(id, cidade, quantidade) values (${id},${cidade},${quantidade})`
+const postPesca = (req,res) => {
+    let body = req.body
+    let string = 'insert into pesca(cidade, quantidade) values (\'' + body.cidade + '\',' + body.quantidade +')' 
+    console.log(string)
     con.query(string, (err, result)=>{
         if(err != null){
             res.status(400).end()
         }else{
-            res.status(201).end()
+            res.status(200).end()
         }
     })
 }
-
-const putPesca = (req, res) => {
-
+const putPesca = (req,res) => {
     let id = req.body.id
-    let cidade = '\'' + req.body.cidade + '\''
-    let quantidade = req.body.quantidade
-
-    let string = `update pesca set cidade = ${cidade}, quantidade = ${quantidade} where id = ${id}`
+    let cidade = req.body.cidade;
+    let quantidade = req.body.quantidade;
+    let string = `update pesca set cidade = '${cidade}', quantidade = ${quantidade} where id = ${id}`
     con.query(string, (err, result)=>{
-        if(result.affectedRows == 0 ){
-            res.status(404).end()
-        }else{
-            con.query('select * from pesca where id = ' + id,(err, result)=>{
-                res.json(model.pescaModel(result[0]))
+        if(result.affectedRows == 1){
+            con.query('select * from pesca where id ='+id, (err, result)=>{
+                res.json(result)
             })
+        }else{
+            res.send(400).end()
         }
     })
 }
-
 
 const deletePesca = (req,res) => {
     let string = 'delete from pesca where id = ' + req.params.id
     con.query(string, (err, result)=>{
-       if(result.affectedRows == 0){
-           res.send(400).end()
-       }else{
-           res.send(200).end()
-       }
+        if(result.affectedRows == 0){
+            req.send(400).end()
+        }else{
+            res.send(200).end()
+        }
     })
 }
 
+
+
 module.exports = {
-    getPesca,
-    getPescaId,
+    all,
+    get_id,
     postPesca,
     putPesca,
-    deletePesca,
+    deletePesca
 }
