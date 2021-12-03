d159173f7f786ffdc929243a9b16773adf0422c7
Gustavo Henrique <82879034+Gustavo010@users.noreply.github.com>
2021-12-03 17:00:56 -0300

    Add files via upload

---
+++ pescar.sql
@@ -0,0 +1,11 @@
+drop database if exists Pescaria;
+create database Pescaria;
+use Pescaria;
+
+create table Pesca(
+    id  integer primary key auto_increment,
+    cidade varchar(100) not null,
+    quantidade decimal (9,2) not null
+);
+
+insert into Pesca(cidade, quantidade) values ('Santos', 120),('Ubatuba', 150), ('Pedreira', 20);
